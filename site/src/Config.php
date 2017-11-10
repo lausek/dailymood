44107c81e49b75ff878cca1f9c63add8d2ae4888
lausek <root@lausek.eu>
2017-11-10 14:48:56 +0100

    basic configuration

--- site/src/Config.php
+++ site/src/Config.php
@@ -2,4 +2,20 @@
 
 class Config {
     
+	private static const PATH_CONFIG_SERVICE = "";
+	
+	private static $initialized = false;
+	private static $obj = null;
+	
+	public static function init() {
+		if(!self::$initialized) {
+			$obj = yaml_parse_file(PATH_CONFIG_SERVICE);
+			self::$initialized = true;
+		}
+	}
+	
+	public static function get($attr) {
+		return self::$obj[$attr];
+	}
+	
 }
