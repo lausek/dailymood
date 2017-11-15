cc5ef1eae77fad3d12176fe2d517605b2b013a6c
lausek <root@lausek.eu>
2017-11-15 10:44:04 +0100

    implemented pepper

--- site/src/User.php
+++ site/src/User.php
@@ -14,21 +14,25 @@ class User {
         session_start();
         
         if(isset($_SESSION["user"])) {
-        	return unserialize($_SESSION["user"]);
-        } elseif(isset($_POST["username"]) && isset($_POST["password"])) {
-        	// TODO: select user
         	
-        	$stmt = DataActor::get()->prepare("SELECT id FROM users WHERE name = ? AND password = SHA1(CONCAT(?, users.salt))");
+        	return unserialize($_SESSION["user"]);
+        	
+        } elseif(isset($_POST["username"]) && isset($_POST["password"])) {
+        	
+        	$stmt = DataActor::get()->prepare("SELECT id FROM users WHERE name = ? AND password = SHA1(CONCAT(?, users.salt, ?))");
         	$stmt->bindValue(1, $_POST["username"]);
         	$stmt->bindValue(2, $_POST["password"]);
+        	$stmt->bindValue(3, Config::get("pepper"));
         	
         	if(!$stmt->execute()) {
+        		http_response_code(401);
         		return null;
         	}
         	
         	$user = new User($stmt->fetchColumn(0), $_POST["username"]);
         	$_SESSION["user"] = serialize($user);
         	return $user;
+        	
         }
 
         return null;
