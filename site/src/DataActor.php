be25579edbab59657b8cc0c783b9ca7c215da47c
lausek <root@lausek.eu>
2017-11-15 14:33:23 +0100

    first version of corrected date array

--- site/src/DataActor.php
+++ site/src/DataActor.php
@@ -28,9 +28,56 @@ class DataActor {
 	
 	public static function get_days($user) {
 		
-		$stmt = self::get()->prepare("SELECT * FROM days WHERE ");
+		$stmt = self::get()->prepare("SELECT * FROM days WHERE user = ? AND day BETWEEN ? AND ? ORDER BY day DESC");
+		$stmt->bindValue(1, $user->get_id(), PDO::PARAM_INT);
+		$stmt->bindValue(2, (new DateTime)->sub(new DateInterval("P30D"))->format("Y-m-d"), PDO::PARAM_STR);
+		$stmt->bindValue(3, (new DateTime)->format("Y-m-d"), PDO::PARAM_STR);
 		
-		$days = $stmt->fetchAll();
+		// nehme erstes datum
+			// wenn bdatum = null
+				// füge tag hinzu
+			// else
+				// wenn bdatum-1 == day["day"]
+					// füge tag hinzu
+				// else
+					// append_empty(bdatum-day["day"])
+			// bdatum = day["day"]
+		// append_empty(bdatum-day["day"])
+			
+		if(!$stmt->execute()) {
+			foreach($stmt->errorInfo() as $msg) {
+				echo $msg."<br>";
+			}
+			return [];
+		}
+		
+		$days = [];
+		
+		$row = $stmt->fetch(PDO::FETCH_ASSOC);
+		$lastDate = $row !== false ? new DateTime($row["day"]) : null;		
+		
+		for($i = 1; $i <= 30; $i++) {
+			
+			$expected = (new DateTime)->sub(new DateInterval("P".($i-1)."D"));
+			
+// 			echo "LastDate: ".$lastDate->format("Y-m-d")." Expected:".$expected->format("Y-m-d")."<br>";
+// 			echo var_dump($lastDate == $expected);
+			
+			if($lastDate === null
+			|| $lastDate->format("Y-m-d") != $expected->format("Y-m-d")) {
+				$days[$i] = null;
+			}else{
+				$days[$i] = $row;
+				$row = $stmt->fetch(PDO::FETCH_ASSOC);
+				$lastDate = $row !== false ? new DateTime($row["day"]) : null;
+			}
+			
+		}
+		
+// 		echo '<pre>';
+// 		echo var_dump($days);
+// 		echo '</pre>';
+// 		exit;
 		
 		return $days;
 		
